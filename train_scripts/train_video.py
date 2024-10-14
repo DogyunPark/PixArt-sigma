ce70d9ed535cacd3501f90b945ada95390412ca2
koparkrea <koparkrea@gmail.com>
2024-10-14 19:12:25 +0900

    add

--- train_scripts/train_video.py
+++ train_scripts/train_video.py
@@ -203,9 +203,9 @@ def train():
 
                 info = f"Step/Epoch [{global_step}/{epoch}][{step + 1}/{len(train_dataloader)}]:total_eta: {eta}, " \
                     f"epoch_eta:{eta_epoch}, time_all:{t:.3f}, time_data:{t_d:.3f}, lr:{lr:.3e}, "
-                info += f's:({model.module.h}, {model.module.w}), ' if hasattr(model, 'module') else f's:({model.h}, {model.w}), '
+                #info += f's:({model.module.h}, {model.module.w}), ' if hasattr(model, 'module') else f's:({model.h}, {model.w}), '
 
-                info += ', '.join([f"{k}:{v:.4f}" for k, v in log_buffer.output.items()])
+                #info += ', '.join([f"{k}:{v:.4f}" for k, v in log_buffer.output.items()])
                 logger.info(info)
                 last_tic = time.time()
                 log_buffer.clear()
