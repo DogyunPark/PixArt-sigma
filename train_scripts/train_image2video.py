3b563462cda81427296d210e41550b9f5eb7fdf6
koparkrea <koparkrea@gmail.com>
2024-10-15 18:14:22 +0900

    add

--- train_scripts/train_image2video.py
+++ train_scripts/train_image2video.py
@@ -157,11 +157,11 @@ def train():
                         if p > config.image_dropout_prob:
                             first_frame_cond = torch.zeros_like(x)
                             first_frame_cond[:,:,0] = x[:,:,0].detach().clone()
-                            first_frame_mask = torch.zeros((b1, 1, t1, h1, w2)).to(accelerator.device, torch.float16)
+                            first_frame_mask = torch.zeros((b1, 1, t1, h1, w1)).to(accelerator.device, torch.float16)
                             first_frame_mask[:,:,0] = 1.
                         else:
                             frist_frame_cond = torch.zeros_like(x)
-                            first_frame_mask = torch.zeros((b1, 1, t1, h1, w2)).to(accelerator.device, torch.float16)
+                            first_frame_mask = torch.zeros((b1, 1, t1, h1, w1)).to(accelerator.device, torch.float16)
                         
                         x = torch.cat([x, first_frame_cond, first_frame_mask], axis=1)
                         
