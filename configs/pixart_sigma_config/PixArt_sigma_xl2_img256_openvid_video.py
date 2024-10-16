83895160e65b3fdb515e4fab1d75f18f04988361
koparkrea <koparkrea@gmail.com>
2024-10-16 14:34:44 +0900

    flow add

--- configs/pixart_sigma_config/PixArt_sigma_xl2_img256_openvid_video.py
+++ configs/pixart_sigma_config/PixArt_sigma_xl2_img256_openvid_video.py
@@ -35,6 +35,7 @@ optimizer = dict(type='AdamWWrapper', lr=2e-5, weight_decay=0.03, betas=(0.9, 0.
 lr_schedule_args = dict(num_warmup_steps=1000)
 image_dropout_prob = 0.1
 weight_freeze = "not_temporal_and_lastlayer"
+reparameterization = True
 
 eval_sampling_steps = 150
 visualize = True
